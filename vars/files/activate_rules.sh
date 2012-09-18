10f75f1451c93d844810ee4d0e06dcddfd1e5d22
getintouch@icomefromthenet.com <getintouch@icomefromthenet.com>
2012-09-18 16:44:24 +1000

    removed out of date files

--- vars/files/activate_rules.sh
+++
@@ -1,5 +0,0 @@
-for f in `ls /etc/apache2/modsecurity-crs/base_rules/`;
-    do
-      ln -s /etc/apache2/modsecurity-crs/base_rules/$f /etc/apache2/modsecurity-crs/activated_rules/$f ;
-    done
-exit 0;
